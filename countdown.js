f2af609788e6f709caa00b91c46d13849980b22b
EBA <official-eba@outlook.com>
2021-06-16 11:52:22 -0400

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,17 +1,8 @@
+var i = document.getElementById('counter');
 function countdown() {
-  var i = document.getElementById('counter');
   if (parseInt(i.innerHTML)=0) {
     countdownExe();
   }
   i.innerHTML = parseInt(i.innerHTML)-1;
-  countdownSec();
-}
-function countdownSec() {
-  var x = document.getElementById('sec');
-  if (parseInt(i.innerHTML)=1) {
-    x.innerHTML = 'second';
-  } else {
-    x.innerHTML = 'seconds';
-  }
 }
 setInterval(function(){ countdown(); },1000);
